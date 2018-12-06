8e8d94cae051cb32b91d1374f02dcdb81ed5094e
Joonas Nahkala <h8636@student.jamk.fi>
2018-12-06 21:41:00 +0200

    add assets

--- js/scrollnav.js
+++ js/scrollnav.js
@@ -1,7 +1,7 @@
 (function($) {          
           $(document).ready(function(){                    
               $(window).scroll(function(){                          
-                  if ($(this).scrollTop() > 200) {
+                  if ($(this).scrollTop() > 170) {
                       $('nav').fadeIn(130);
                   } else {
                       $('nav').fadeOut(110);
