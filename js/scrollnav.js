31c7c8201f7b2440a5f822f426983e05cfaf557d
Joonas Nahkala <joonas.nahkala@gmail.com>
2018-12-07 10:47:47 +0200

    add js

--- js/scrollnav.js
+++ js/scrollnav.js
@@ -1,11 +1,18 @@
-(function($) {          
-          $(document).ready(function(){                    
-              $(window).scroll(function(){                          
-                  if ($(this).scrollTop() > 170) {
-                      $('nav').fadeIn(130);
-                  } else {
-                      $('nav').fadeOut(110);
-                  }
-              });
-          });
-})(jQuery);
+(function($) {
+    $(document).ready(function() {
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+          $('nav').fadeIn(130);
+        } else {
+          $('nav').fadeOut(110);
+        }
+        // Button scroll
+        if ($(this).scrollTop() > 980) {
+          $('button').addClass('butout');
+        } else {
+          $('button').removeClass('butout');
+        }
+      });
+    });
+  })(jQuery);
+  
